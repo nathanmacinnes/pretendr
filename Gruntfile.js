53506ca4102bade547ba796dea017ad031afb8ef
Nathan MacInnes <nathan@macinn.es>
2016-07-25 08:36:37 +0000

    Changed gruntfile indent
    We might actually get a passing travis build!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,62 @@
 module.exports = function (grunt) {
 
-    // Project configuration
-    grunt.initConfig({
-        pkg : grunt.file.readJSON("package.json"),
-        mochaTest : {
-            test : {
-                options : {
-                    reporter : "spec"
-                },
-                src : ["test/*.js"]
-            }
+  // Project configuration
+  grunt.initConfig({
+    pkg : grunt.file.readJSON("package.json"),
+    mochaTest : {
+      test : {
+        options : {
+          reporter : "spec"
         },
-        jshint : {
-            core : ["Gruntfile.js"],
-            test : ["test/*.js"],
-            lib : ["lib/*.js"],
-            options : {
-                jshintrc : true
-            }
-        },
-        jscs : {
-            lib : {
-                src : "lib/*.js",
-            },
-            test : {
-                src : "test/*.js"
-            },
-            misc : {
-                src : ["Gruntfile.js"]
-            },
-            options : {
-                config : ".jscsrc"
-            }
-        },
-        watch : {
-            lib : {
-                files : ["lib/*.js"],
-                tasks : ["test", "jshint:lib", "jscs:lib"]
-            },
-            test : {
-                files : ["test/*.js"],
-                tasks : ["test", "jshint:test", "jscs:test"]
-            },
-            grunt : {
-                files : ["Gruntfile.js"],
-                tasks : ["jshint:core"]
-            }
-        }
-    });
+        src : ["test/*.js"]
+      }
+    },
+    jshint : {
+      core : ["Gruntfile.js"],
+      test : ["test/*.js"],
+      lib : ["lib/*.js"],
+      options : {
+        jshintrc : true
+      }
+    },
+    jscs : {
+      lib : {
+        src : "lib/*.js",
+      },
+      test : {
+        src : "test/*.js"
+      },
+      misc : {
+        src : ["Gruntfile.js"]
+      },
+      options : {
+        config : ".jscsrc"
+      }
+    },
+    watch : {
+      lib : {
+        files : ["lib/*.js"],
+        tasks : ["test", "jshint:lib", "jscs:lib"]
+      },
+      test : {
+        files : ["test/*.js"],
+        tasks : ["test", "jshint:test", "jscs:test"]
+      },
+      grunt : {
+        files : ["Gruntfile.js"],
+        tasks : ["jshint:core"]
+      }
+    }
+  });
 
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-mocha-test");
-    grunt.loadNpmTasks("grunt-jscs");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-mocha-test");
+  grunt.loadNpmTasks("grunt-jscs");
 
-    grunt.registerTask("test", "mochaTest");
-    grunt.registerTask("lint", ["jshint", "jscs"]);
+  grunt.registerTask("test", "mochaTest");
+  grunt.registerTask("lint", ["jshint", "jscs"]);
 
-    grunt.registerTask("default", ["test", "lint"]);
+  grunt.registerTask("default", ["test", "lint"]);
 
-    grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 };
