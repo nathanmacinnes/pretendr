b35e997ac0640fa48ed6cf4a6a97c31ee490b2e5
nathanmacinnes <nathan@macinn.es>
2021-03-09 13:17:50 +0000

    Bracket notation in tests, as a little more compact

--- test/spec.js
+++ test/spec.js
@@ -4,7 +4,7 @@ const chance = require("chance-multiseed");
 const expect = require("expect.js");
 const injectr = require("injectr");
 
-describe("pretendr", function () {
+describe("pretendr", () => {
   let random;
   let generateArguments;
   let pretendr;
@@ -12,22 +12,22 @@ describe("pretendr", function () {
     random = chance(this.currentTest.title);
     pretendr = injectr("../lib/pretendr.js");
 
-    generateArguments = function () {
+    generateArguments = () => {
       return random.n(random.string, random.natural({
         min : 1,
         max : 20
       }));
     };
   });
-  it("is a function", function () {
+  it("is a function", () => {
     expect(pretendr).to.be.a("function");
   });
-  it("returns an object with a mock property", function () {
+  it("returns an object with a mock property", () => {
     expect(pretendr()).to.be.an("object")
       .and.to.have.property("mock");
   });
-  it("returns a pretendr function when no arguments are given", function () {
-    const withFn = pretendr(function () {});
+  it("returns a pretendr function when no arguments are given", () => {
+    const withFn = pretendr(() => {});
     const withNoArgs = pretendr();
 
     // eql won't work because it expects functions to be ===
@@ -35,41 +35,41 @@ describe("pretendr", function () {
       expect(withFn[key].toString()).to.equal(withNoArgs[key].toString());
     });
   });
-  it("returns an undefined pretendr when undefined is given", function () {
+  it("returns an undefined pretendr when undefined is given", () => {
     expect(pretendr(undefined)).to.have.property("mock", undefined);
   });
-  it("returns a null pretendr when null is given", function () {
+  it("returns a null pretendr when null is given", () => {
     expect(pretendr(null)).to.have.property("mock", null);
   });
-  describe("with function", function () {
+  describe("with function", () => {
     let callback;
     let p;
-    beforeEach(function () {
+    beforeEach(() => {
 
       // dogfood
       callback = pretendr();
       p = pretendr();
     });
-    it("has an empty calls array", function () {
+    it("has an empty calls array", () => {
       expect(p.calls).to.be.eql([]);
     });
-    it("mocks its properties", function () {
-      const fn = function () {};
+    it("mocks its properties", () => {
+      const fn = () => {};
       const property = random.string();
-      fn[property] = function () {};
+      fn[property] = () => {};
       p = pretendr(fn);
       p.mock[property]();
       expect(p[property].calls).to.have.length(1);
     });
-    describe("has a mock property which", function () {
-      it("is a function", function () {
+    describe("has a mock property which", () => {
+      it("is a function", () => {
         expect(p.mock).to.be.a("function");
       });
-      it("records its calls", function () {
+      it("records its calls", () => {
         p.mock();
         expect(p.calls).to.have.length(1);
       });
-      it("records the arguments to each call", function () {
+      it("records the arguments to each call", () => {
         const args = random.n(random.integer, random.natural({
           min : 1,
           max : 20
@@ -77,48 +77,48 @@ describe("pretendr", function () {
         p.mock.apply(null, args);
         expect(p.calls[0].args).to.eql(args);
       });
-      it("records the context of each call", function () {
+      it("records the context of each call", () => {
         const context = {};
         p.mock.call(context);
         expect(p.calls[0]).to.have.property("context", context);
       });
-      it("records the return value of fakes", function () {
+      it("records the return value of fakes", () => {
         const ret = {};
         p.fake(callback.mock);
         callback.returnValue(ret);
         p.mock();
         expect(p.calls[0]).to.have.property("returned", ret);
       });
-      it("finds the callback", function () {
+      it("finds the callback", () => {
         const args = generateArguments().concat(callback.mock)
           .concat(generateArguments());
         p.mock.apply(null, args);
         expect(p.calls[0]).to.have.property("callback", callback.mock);
       });
     });
-    describe("has a returnValue method which", function () {
-      it("sets the return value", function () {
+    describe("has a returnValue method which", () => {
+      it("sets the return value", () => {
         const v = {};
         p.returnValue(v);
         expect(p.mock()).to.equal(v);
       });
-      it("can retrieve the return value", function () {
+      it("can retrieve the return value", () => {
         p.returnValue({});
         expect(p.returnValue()).to.equal(p.mock());
       });
-      it("sets a return value of undefined when told to", function () {
+      it("sets a return value of undefined when told to", () => {
         p.returnValue({});
         p.returnValue(undefined);
         expect(p.returnValue()).to.equal(undefined);
       });
     });
-    describe("has a fake method which", function () {
-      it("sets a callback function to run", function () {
+    describe("has a fake method which", () => {
+      it("sets a callback function to run", () => {
         p.fake(callback.mock);
         p.mock();
         expect(callback.calls).to.have.length(1);
       });
-      it("passes arguments to the callback", function () {
+      it("passes arguments to the callback", () => {
         const args = random.n(random.integer, random.natural({
           min : 1,
           max : 20
@@ -127,33 +127,33 @@ describe("pretendr", function () {
         p.mock.apply(null, args);
         expect(callback.calls[0].args).to.eql(args);
       });
-      it("passes the context to the callback", function () {
+      it("passes the context to the callback", () => {
         const context = {};
         p.fake(callback.mock);
         p.mock.call(context);
         expect(callback.calls[0].context).to.equal(context);
       });
-      it("makes the mock return the fake's return value", function () {
+      it("makes the mock return the fake's return value", () => {
         const ret = {};
         p.fake(callback.mock);
         callback.returnValue(ret);
         expect(p.mock()).to.equal(ret);
       });
     });
-    describe("with template", function () {
-      it("returns a new mock based on the template", function () {
+    describe("with template", () => {
+      it("returns a new mock based on the template", () => {
         p.template({
-          a : function () {}
+          a : () => {}
         });
         expect(p.mock().a).to.be.a("function");
       });
-      it("doesn't return the same object twice", function () {
+      it("doesn't return the same object twice", () => {
         p.template({});
         expect(p.mock()).to.not.equal(p.mock());
       });
-      it("applies fakes appropriately", function () {
+      it("applies fakes appropriately", () => {
         const templateObj = p.template({
-          a : function () {}
+          a : () => {}
         });
         templateObj.a.fake(callback.mock);
         const res = p.mock();
@@ -161,33 +161,33 @@ describe("pretendr", function () {
         res.a();
         expect(callback.calls).to.have.length(2);
       });
-      it("saves the template instance", function () {
-        const template = p.template(function () {});
+      it("saves the template instance", () => {
+        const template = p.template(() => {});
         const res = p.mock();
         expect(template.instances[0]).to.have.property("mock", res);
       });
-      it("also saves the template instance to the call", function () {
-        const template = p.template(function () {});
+      it("also saves the template instance to the call", () => {
+        const template = p.template(() => {});
         p.mock();
         expect(template.instances[0]).to.equal(p.calls[0].pretendr);
       });
-      it("should be able to return the template", function () {
+      it("should be able to return the template", () => {
         const template = p.template({});
         expect(p.template()).to.equal(template);
       });
     });
-    describe("as constructor", function () {
-      it("should also have a Mock method", function () {
+    describe("as constructor", () => {
+      it("should also have a Mock method", () => {
         expect(p.Mock).to.equal(p.mock);
       });
-      it("should be a genuine instance", function () {
+      it("should be a genuine instance", () => {
         expect(new p.Mock()).to.be.a(p.Mock);
       });
-      it("should save a pretendr object to instance", function () {
+      it("should save a pretendr object to instance", () => {
         const obj = new p.Mock();
         expect(p.instances[0]).to.have.property("mock", obj);
       });
-      it("should apply a template to the instance", function () {
+      it("should apply a template to the instance", () => {
         const templateDescriptor = {
           a : random.string()
         };
@@ -195,17 +195,17 @@ describe("pretendr", function () {
         const obj = new p.Mock();
         expect(obj).to.have.property("a", templateDescriptor.a);
       });
-      it("makes the instance equal to the call", function () {
+      it("makes the instance equal to the call", () => {
         new p.Mock();
         expect(p.instances[0]).to.equal(p.calls[0]);
       });
-      it("sets the asConstructor property to true", function () {
+      it("sets the asConstructor property to true", () => {
         new p.Mock();
         expect(p.calls[0]).to.have.property("asConstructor", true);
       });
     });
-    describe("findCall method", function () {
-      it("finds the call with the matching number of arguments", function () {
+    describe("findCall method", () => {
+      it("finds the call with the matching number of arguments", () => {
         createCalls(
           [1, "a", true],
           [1, "a"],
@@ -214,28 +214,28 @@ describe("pretendr", function () {
         expect(p.findCall(2)).to.equal(p.calls[1]);
         expect(p.findCall(0)).to.equal(p.calls[2]);
       });
-      it("finds the call with matching arguments", function () {
+      it("finds the call with matching arguments", () => {
         createCalls(
           [1, "a", true],
           [2, "b", false],
           [2, "b", true]);
         expect(p.findCall([2, "b", false])).to.equal(p.calls[1]);
       });
-      it("treats null as any value", function () {
+      it("treats null as any value", () => {
         createCalls([3, 4, 5]);
         expect(p.findCall([3, null, 5])).to.equal(p.calls[0]);
       });
-      describe("with function", function () {
+      describe("with function", () => {
         let args = ["one", "two", "three", "four", "five", "six"];
         let fn;
-        beforeEach(function () {
+        beforeEach(() => {
           const copy = Array.from(args);
           fn = pretendr();
           while (copy.length) {
             createCalls(copy.splice(0, 2));
           }
         });
-        it("can call the function once for each arg", function () {
+        it("can call the function once for each arg", () => {
           fn.fake(function (e, index, arr) {
             return index !== arr.length - 1;
           });
@@ -246,7 +246,7 @@ describe("pretendr", function () {
             return call.args[0];
           })).to.eql(args);
         });
-        it("returns the call for which all arg calls return true", function () {
+        it("returns the call for which all arg calls return true", () => {
           // I'd like to see you come up with a better description!
           fn.fake(function (val) {
             return val !== "two" && val !== "four";
@@ -263,87 +263,87 @@ describe("pretendr", function () {
       }
     });
   });
-  describe("with an object", function () {
+  describe("with an object", () => {
     let descriptor;
     let p;
-    beforeEach(function () {
+    beforeEach(() => {
       descriptor = {
         stringProperty : random.string(),
         numberProperty : random.floating(),
         booleanProperty : random.bool(),
-        aMethod : function () {}
+        aMethod : () => {}
       };
       p = pretendr(descriptor);
     });
-    it("mocks its methods", function () {
+    it("mocks its methods", () => {
       expect(p.aMethod.mock).to.be.a("function");
     });
-    it("mocks strings", function () {
+    it("mocks strings", () => {
       expect(p.mock.stringProperty).to.eql(descriptor.stringProperty);
     });
-    it("mocks numbers", function () {
+    it("mocks numbers", () => {
       expect(p.mock.numberProperty).to.eql(descriptor.numberProperty);
     });
-    it("mocks booleans", function () {
+    it("mocks booleans", () => {
       expect(p.mock.booleanProperty).to.eql(descriptor.booleanProperty);
     });
-    describe("has a mock property which", function () {
-      it("is an object", function () {
+    describe("has a mock property which", () => {
+      it("is an object", () => {
         expect(p.mock).to.be.an("object");
       });
-      it("doesn't equal the original", function () {
+      it("doesn't equal the original", () => {
         expect(p.mock).to.not.equal(descriptor);
       });
-      it("has corresponding properties for sub-pretendrs", function () {
+      it("has corresponding properties for sub-pretendrs", () => {
         expect(p.mock).to.have.property("aMethod", p.aMethod.mock);
       });
     });
-    describe("for each of its properties", function () {
-      it("has a gets property which is set to 0", function () {
+    describe("for each of its properties", () => {
+      it("has a gets property which is set to 0", () => {
         expect(p.stringProperty.gets).to.equal(0);
       });
-      it("increments the gets property with each retrieve", function () {
+      it("increments the gets property with each retrieve", () => {
         const dummy = p.mock.stringProperty; // jshint ignore:line
         expect(p.stringProperty.gets).to.equal(1);
       });
-      it("has an empty values array", function () {
+      it("has an empty values array", () => {
         expect(p.stringProperty.values).to.eql([]);
       });
-      it("pushes each set to the values array", function () {
+      it("pushes each set to the values array", () => {
         p.mock.stringProperty = random.string();
         expect(p.stringProperty.values[0]).to
           .equal(p.stringProperty.mock);
       });
     });
-    describe("with circular references", function () {
+    describe("with circular references", () => {
       let descriptor;
-      beforeEach(function () {
+      beforeEach(() => {
         descriptor = {};
         descriptor.circ = descriptor;
       });
-      it("mimics the object with its own circular refs", function () {
+      it("mimics the object with its own circular refs", () => {
         p = pretendr(descriptor);
         expect(p.circ).to.equal(p.circ.circ);
       });
-      it("can still create multiple instances", function () {
+      it("can still create multiple instances", () => {
         expect(pretendr(descriptor)).to.not.equal(pretendr(descriptor));
       });
     });
   });
-  describe("with an array", function () {
+  describe("with an array", () => {
     let descriptor;
     let p;
-    beforeEach(function () {
+    beforeEach(() => {
       descriptor = random.n(random.string, random.natural({
         min : 1,
         max : 20
       }));
       p = pretendr(descriptor);
     });
-    it("has an array as the mock", function () {
+    it("has an array as the mock", () => {
       expect(p.mock).to.be.an("array");
     });
-    it("resembles the descriptor", function () {
+    it("resembles the descriptor", () => {
       // need to use join because eql doesn't like the getters/setters
       expect(p.mock.join(",")).to.equal(descriptor.join(","));
     });
